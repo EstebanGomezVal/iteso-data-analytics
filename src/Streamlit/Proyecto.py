a23dc2d1a82516260552c9f5f0d5a811d7f93a93
ValerioEsteban <estebangmzv@gmail.com>
2024-04-29 07:20:19 -0600

    streamlit

--- src/Streamlit/Proyecto.py
+++ src/Streamlit/Proyecto.py
@@ -1,13 +1,4 @@
 import streamlit as st
-import numpy as np
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-import category_encoders as ce
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.preprocessing import StandardScaler
 
 st.title("Visualización del Titanic")
 st.header("Data set")
